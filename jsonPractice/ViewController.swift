1572f22d945c8f61adffc4b3706454c2bf97ccb2
minalborole26 <minalborole2611@gmail.com>
2023-08-21 17:31:18 +0530

    push changes

---
+++ jsonPractice/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  jsonPractice
+//
+//  Created by Minal Borole on 19/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //https://jsonplaceholder.typicode.com/todos
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+
+
+}
+
